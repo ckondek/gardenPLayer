575a1820f28df3c8b6d01bdebb6376c6efdc580c
ckondek <chef@loanshark.de>
2020-09-15 22:16:53 +0200

    Fixed repeated Load problem and last updated

--- view1/scripts/script.js
+++ view1/scripts/script.js
@@ -3,18 +3,15 @@ window.onload = function(){
   var canvas = document.getElementById('myCanvas');
   var context = canvas.getContext('2d');
   canvas.width=window.innerWidth;
-  canvas.height=window.innerHeight-10;
+  canvas.height=window.innerHeight;
+  context.fillStyle = "black";
   screenRatio=canvas.width/canvas.height
-
   var video  = document.getElementById('videoTag');
-  var activator =document.getElementById('activate');
-  var test =document.getElementById('test');
-  var counter=0;
-  var imgObj;
-
+  var counter=8;
+  var imgObj=new Image();;
   var workList;
   var lastUpdated;
-  var currentLastUpdated=0;
+  var currentLastUpdated="2020-09-15T11:49:14.353296Z";
   var currentIndex=0;
   var videoArray=[
               'http://d1ua7ibh28zfpl.cloudfront.net/gardenFilms/grow1.mp4',
@@ -22,45 +19,22 @@ window.onload = function(){
               'http://d1ua7ibh28zfpl.cloudfront.net/gardenFilms/grow3.mp4'
             ];
 
-  //video.src=videoArray[2];
-  activator.addEventListener("click",function(){
-    video.style.display="block";
-    activator.style.display = "none";
-    video.play();
-    video.addEventListener('ended', function () {
-    video.style.display="none";
-    });
-  });
 
   function triggerRandomVideo(){
-    video.style.display="block";
-    var choice=Math.floor(Math.random()*videoArray.length);
-    video.src=videoArray[choice];
-    video.load()
-    video.addEventListener('canplaythrough',function(){
-      video.play();
-    })
-    video.addEventListener('ended', function () {
-      video.style.display="none";
-      mainLoop()
-    });
-  }
+      console.log("video sequence Triggered")
+      video.style.display="block";
 
-  function reset(){
-    garden=getDB();
-    workList=[];
-    currentIndex=0;
-    for (var item=0;item<garden.length;item++){
-      if (getGarden(garden[item])==2){
-        workList.push(entryDict(garden[item]));
-        console.log(entryDict(garden[item]))
-      }
-    }
+      var choice=Math.floor(Math.random()*videoArray.length);
+      video.src=videoArray[choice];
+      video.play();
+      context.fillRect(0, 0, canvas.width, canvas.height);
+
   }
   function makeAspect(){
     let imageRatio=imgObj.width/imgObj.height;
     let newWidth=imgObj.width;
     let newHeight=imgObj.height;
+
     if (imageRatio < screenRatio){
       newHeight=imgObj.width/screenRatio;
     }else if (imageRatio > screenRatio){
@@ -69,76 +43,83 @@ window.onload = function(){
       return [newWidth,newHeight]
   }
 
+  video.addEventListener('ended', function () {
+        video.style.display="none";
+        counter+=1;
+        console.log("Video end is called")
+        mainLoop();
+      });
 
-  function getIndex(){
-      console.log("getting index")
-   workList=getDB()
-   console.log(workList)
+  imgObj.onload = function(){
+      console.log("New image loaded")
+       let newSizes=makeAspect()
+       context.drawImage(imgObj, imgObj.width/2-newSizes[0]/2,imgObj.height/2-newSizes[1]/2,newSizes[0],newSizes[1],0, 0,canvas.width, canvas.height);
 
-   lastUpdated=workList[0]['created_at'];
-   console.log(currentLastUpdated)
-   console.log(lastUpdated)
+       text1.innerHTML="Data Garden at Nantesbuch "+workList[currentIndex]["uuid"];
+
+       if (workList[currentIndex]["original_created_at"]==null){
+         text2.innerHTML="Original created at: an uncertain time in the past";
+       }else{
+         text2.innerHTML="Original created at: "+workList[currentIndex]["original_created_at"];
+       }
+
+       text3.innerHTML="Image planted in garden: "+workList[currentIndex]["created_at"];
+
+       var gen=parseInt(workList[currentIndex]["generation"])+1
+       text4.innerHTML="This Image is generation: "+gen;
+
+        timer =setTimeout(function(){
+         counter+=1;
+         console.log("Image timeout is called.")
+         console.log("About to call mainLoop")
+         mainLoop()
+       },7000);
+     }
+
+  function getImage(){
+
+    workList=getDB()
+    lastUpdated=workList[0]['created_at'];
+    console.log(lastUpdated>currentLastUpdated)
+    console.log(lastUpdated)
+      console.log(lastUpdated)
+
    if (lastUpdated>currentLastUpdated){
      currentLastUpdated=lastUpdated
+
      currentIndex=0;
-       console.log("setting up last")
+
+    console.log("New image found. Will load most recent update")
    }else{
      currentIndex=Math.floor(Math.random()*workList.length)
+     console.log("No new image")
      console.log("Picking Random")
    }
- }
-  function getImage(){
-    console.log("entering get Image")
-    imgObj= new Image();
+    console.log("Current Image index is: "+currentIndex)
+
+    console.log("Image object created")
+    console.log("Current Image inside getImage is index is: "+currentIndex)
+
     imgObj.src = workList[currentIndex]["image_file"];
-    text1.innerHTML="Data Garden at Nantesbuch "+workList[currentIndex]["uuid"];
+    console.log("Image.src added")
+    console.log("src is: "+workList[currentIndex]["image_file"])
 
-    if (workList[currentIndex]["original_created_at"]==null){
-     text2.innerHTML="Original created at: an uncertain time in the past";
-    }else{
-     text2.innerHTML="Original created at: "+workList[currentIndex]["original_created_at"];
-    }
 
-    text3.innerHTML="Image planted in garden: "+workList[currentIndex]["created_at"];
-    text4.innerHTML="This Image is generation: "+workList[currentIndex]["generation"];
-    imgObj.onload = function(){
-        console.log("New image loaded")
-         let newSizes=makeAspect()
-         context.drawImage(imgObj, imgObj.width/2-newSizes[0]/2,imgObj.height/2-newSizes[1]/2,newSizes[0],newSizes[1],0, 0,canvas.width, canvas.height);
-
-         text1.innerHTML="Data Garden at Nantesbuch "+workList[currentIndex]["uuid"];
-         if (workList[currentIndex]["original_created_at"]==null){
-           text2.innerHTML="Original created at: an uncertain time in the past";
-         }else{
-           text2.innerHTML="Original created at: "+workList[currentIndex]["original_created_at"];
-         }
-
-         text3.innerHTML="Image planted in garden: "+workList[currentIndex]["created_at"];
-         var gen=parseInt(workList[currentIndex]["generation"])+1
-
-         text4.innerHTML="This Image is generation: "+gen;
-  }
-
-};
+  };
   function mainLoop(){
-    console.log('--------------------')
-    console.log("start of main loop")
-    counter+=1;
+    console.log("------------------*********-----------------------------------")
+    console.log("back in main loop")
+    console.log("The counter is: "+counter)
     if (counter%10==0){
-      triggerRandomVideo()
-      console.log("endof main loop---Video")
-      console.log("--------------------")
-
+      console.log("_________")
+      console.log("Calling triggerVideo()")
+      triggerRandomVideo();
     }else{
-      getIndex();
+      console.log("_________")
+      console.log("Calling getImage()")
       getImage();
-
-      setTimeout(function(){
-        console.log("endof main loop---Image")
-        console.log("--------------------")
-        mainLoop()
-      },10000);
     }
+    console.log("do we every get here?")
   };
 
 mainLoop();
